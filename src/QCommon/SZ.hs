ef9364ba6635e33c6369d23bb41d946e9f7af269
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 23:21:09 +0200

    SZ: init and clear

--- src/QCommon/SZ.hs
+++ src/QCommon/SZ.hs
@@ -1,3 +1,17 @@
 module QCommon.SZ where
 
+import Data.Word (Word8)
+import qualified Data.Vector.Unboxed as UV
 
+import Quake
+import QCommon.SizeBufT
+
+init :: UV.Vector Word8 -> Int -> (SizeBufT -> Quake a) -> Quake a
+init bufData len f =
+    let buf = SizeBufT False False bufData len 0 0
+    in f buf
+
+clear :: SizeBufT -> (SizeBufT -> Quake a) -> Quake a
+clear buf f =
+    let clearedBuf = buf { _sbCurSize = 0, _sbOverflowed = False }
+    in f clearedBuf
