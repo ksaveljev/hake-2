ac5a28bef18927214b80fba1420e15ea3a7980cf
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 22:50:41 +0200

    Add cmd_text to Globals

--- src/Globals.hs
+++ src/Globals.hs
@@ -9,13 +9,18 @@ module Globals ( Globals
                ) where
 
 import Control.Lens (makeLenses)
+import qualified Data.Vector.Unboxed as UV
 
 import Game.CVarT
+import QCommon.SizeBufT
 
-data Globals = Globals { _curtime   :: Int
-                       , _dedicated :: CVarT
-                       , _nostdout  :: CVarT
-                       }
+data Globals =
+  Globals { _curtime   :: Int
+          , _dedicated :: CVarT
+          , _nostdout  :: CVarT
+
+          , _cmdText   :: SizeBufT
+          }
 
 makeLenses ''Globals
 
@@ -24,4 +29,6 @@ defaultGlobals =
   Globals { _curtime   = 0
           , _dedicated = CVarT "" "" "" 0 True 0.0 Nothing -- doesn't matter as it gets set in Main
           , _nostdout  = CVarT "" "" "" 0 True 0.0 Nothing -- doesn't matter as it gets set in Main
+          
+          , _cmdText   = SizeBufT False False UV.empty 0 0 0
           }
