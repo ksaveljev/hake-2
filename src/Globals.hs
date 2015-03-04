49770cd6a236c3128d00764e836cf00f0626719f
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 17:00:43 +0200

    makeLenses for Globals and QuakeState

--- src/Globals.hs
+++ src/Globals.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Globals where
+
+import Control.Lens (makeLenses)
 
 import Game.CVarT
 
 data Globals = Globals { _dedicated :: CVarT
                        }
+
+makeLenses ''Globals
--- src/QuakeState.hs
+++ src/QuakeState.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 module QuakeState where
+
+import Control.Lens (makeLenses)
 
 import Globals
 
 data QuakeState = QuakeState { _globals :: Globals
                              }
+
+makeLenses ''QuakeState
