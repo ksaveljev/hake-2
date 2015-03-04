a783891795b27a76614d7d93e677108c4c0cce06
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 23:39:23 +0200

    CBuf: add init method

--- src/QCommon/CBuf.hs
+++ src/QCommon/CBuf.hs
@@ -1,9 +1,18 @@
 module QCommon.CBuf where
 
+import Control.Lens
+import Control.Monad.State
+
+import qualified Data.Vector.Unboxed as UV
+
 import Quake
+import QuakeState
+import qualified QCommon.SZ as SZ
 
 init :: Quake ()
-init = undefined -- TODO
+init = do
+    bufData <- liftM (^.globals.cmdTextBuf) get
+    SZ.init bufData (UV.length bufData) (globals.cmdText .=)
 
 addEarlyCommands :: Bool -> Quake ()
 addEarlyCommands clear = undefined -- TODO
