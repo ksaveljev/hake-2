8b5be4b447e6cac6c069b4bf5d64cea04312060a
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 23:20:53 +0200

    SizeBufT: rename record fields

--- src/QCommon/SizeBufT.hs
+++ src/QCommon/SizeBufT.hs
@@ -6,12 +6,12 @@ import Control.Lens (makeLenses)
 import qualified Data.Vector.Unboxed as UV
 
 data SizeBufT =
-  SizeBufT { _sizeBufAllowOverflow :: Bool
-           , _sizeBufOverflowed    :: Bool
-           , _sizeBufData          :: UV.Vector Word8
-           , _sizeBufMaxSize       :: Int
-           , _sizeBufCurSize       :: Int
-           , _sizeBufReadCount     :: Int
+  SizeBufT { _sbAllowOverflow :: Bool
+           , _sbOverflowed    :: Bool
+           , _sbData          :: UV.Vector Word8
+           , _sbMaxSize       :: Int
+           , _sbCurSize       :: Int
+           , _sbReadCount     :: Int
            }
 
 makeLenses ''SizeBufT
