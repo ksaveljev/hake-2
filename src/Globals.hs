7572fb56520ea186dffaeac1de73bdfb2d8c5568
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 23:36:35 +0200

    Globals: add cmdTextBuf field

--- src/Globals.hs
+++ src/Globals.hs
@@ -6,9 +6,11 @@ module Globals ( Globals
                , dedicated
                , nostdout
                , cmdText
+               , cmdTextBuf
                , module Game.CVarT
                ) where
 
+import Data.Word (Word8)
 import Control.Lens (makeLenses)
 import qualified Data.Vector.Unboxed as UV
 
@@ -16,20 +18,22 @@ import Game.CVarT
 import QCommon.SizeBufT
 
 data Globals =
-  Globals { _curtime   :: Int
-          , _dedicated :: CVarT
-          , _nostdout  :: CVarT
+  Globals { _curtime    :: Int
+          , _dedicated  :: CVarT
+          , _nostdout   :: CVarT
 
-          , _cmdText   :: SizeBufT
+          , _cmdText    :: SizeBufT
+          , _cmdTextBuf :: UV.Vector Word8
           }
 
 makeLenses ''Globals
 
 defaultGlobals :: Globals
 defaultGlobals =
-  Globals { _curtime   = 0
-          , _dedicated = newCVarT
-          , _nostdout  = newCVarT
+  Globals { _curtime    = 0
+          , _dedicated  = newCVarT
+          , _nostdout   = newCVarT
           
-          , _cmdText   = SizeBufT False False UV.empty 0 0 0
+          , _cmdText    = SizeBufT False False UV.empty 0 0 0
+          , _cmdTextBuf = UV.replicate 8192 0
           }
