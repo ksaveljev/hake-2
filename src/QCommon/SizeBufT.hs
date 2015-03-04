f44a92b00bd8830b4594de1a2cbe95135c3a08e9
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-04 22:43:52 +0200

    SizeBufT: migrate from Int8 to [correct] Word8

--- src/QCommon/SizeBufT.hs
+++ src/QCommon/SizeBufT.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 module QCommon.SizeBufT where
 
-import Data.Int (Int8)
+import Data.Word (Word8)
 import Control.Lens (makeLenses)
 import qualified Data.Vector.Unboxed as UV
 
 data SizeBufT =
   SizeBufT { _sizeBufAllowOverflow :: Bool
            , _sizeBufOverflowed    :: Bool
-           , _sizeBufData          :: UV.Vector Int8
+           , _sizeBufData          :: UV.Vector Word8
            , _sizeBufMaxSize       :: Int
            , _sizeBufCurSize       :: Int
            , _sizeBufReadCount     :: Int
