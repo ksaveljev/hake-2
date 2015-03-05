b0248083dbd1b914ee900b6dc3c3b97151868b19
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-05 12:53:44 +0200

    Cmd: init skeleton

--- src/Game/Cmd.hs
+++ src/Game/Cmd.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Game.Cmd where
+
+import qualified Data.ByteString as B
 
 import Quake
 
 init :: Quake ()
-init = undefined -- TODO
+init = do
+    addCommand "exec" execF
+    addCommand "echo" echoF
+    addCommand "cmdlist" listF
+    addCommand "alias" aliasF
+    addCommand "wait" waitF
+
+addCommand :: B.ByteString -> Quake () -> Quake ()
+addCommand cmdName f = undefined -- TODO
+
+execF :: Quake ()
+execF = undefined -- TODO
+
+echoF :: Quake ()
+echoF = undefined -- TODO
+
+listF :: Quake ()
+listF = undefined -- TODO
+
+aliasF :: Quake ()
+aliasF = undefined -- TODO
+
+waitF :: Quake ()
+waitF = undefined -- TODO
