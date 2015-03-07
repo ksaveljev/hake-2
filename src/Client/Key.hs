ec67ec8eefda6baa4d8751bf37864e6886303639
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-07 15:30:04 +0200

    Key.init

--- src/Client/Key.hs
+++ src/Client/Key.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Client.Key where
 
 import Control.Lens ((.=), (%=))
@@ -7,6 +8,7 @@ import qualified Data.ByteString as B
 
 import Quake
 import QuakeState
+import QCommon.XCommandT
 import Client.KeyConstants
 import qualified Game.Cmd as Cmd
 
@@ -22,9 +24,28 @@ init = do
              kIns, kKpIns, kKpDel, kKpSlash, kKpPlus, kKpMinus, 
              kKp5] `zip` repeat True) ++ [(96, False), (126, False)]))
 
-    undefined -- TODO
+    keyGlobals.menuBound %= (UV.// ([kF1, kF2, kF3, kF4, kF5,
+                             kF6, kF7, kF8, kF9, kF10,
+                             kF11, kF12, kEscape] `zip` repeat True))
+
+    Cmd.addCommand "bind" bindF
+    Cmd.addCommand "unbind" unbindF
+    Cmd.addCommand "unbindall" unbindAllF
+    Cmd.addCommand "bindlist" bindListF
 
   where setupKeyLines = do
           let kl = V.replicate 32 $ B.pack [93, 0] -- 93 is ']', 0 is NUL
           globals.keyLines .= kl
           globals.keyLinePos .= 1
+
+bindF :: XCommandT
+bindF = undefined -- TODO
+
+unbindF :: XCommandT
+unbindF = undefined -- TODO
+
+unbindAllF :: XCommandT
+unbindAllF = undefined -- TODO
+
+bindListF :: XCommandT
+bindListF = undefined -- TODO
