afef6de64028c896108408d910e993c525df04b1
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-07 15:25:09 +0200

    Init consoleKeys in Key.init

--- src/Client/Key.hs
+++ src/Client/Key.hs
@@ -1,16 +1,26 @@
 module Client.Key where
 
-import Control.Lens ((.=))
+import Control.Lens ((.=), (%=))
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
 import qualified Data.ByteString as B
 
 import Quake
 import QuakeState
+import Client.KeyConstants
 import qualified Game.Cmd as Cmd
 
 init :: Quake ()
 init = do
     setupKeyLines
+
+    keyGlobals.consoleKeys %= (UV.// (([32..127] `zip` repeat True) ++ 
+            ([kEnter, kKpEnter, kTab, kLeftArrow, kKpLeftArrow,
+             kRightArrow, kKpRightArrow, kUpArrow, kKpUpArrow,
+             kDownArrow, kKpDownArrow, kBackspace, kHome, kKpHome,
+             kEnd, kKpEnd, kPgUp, kKpPgUp, kPgDn, kKpPgDn, kShift,
+             kIns, kKpIns, kKpDel, kKpSlash, kKpPlus, kKpMinus, 
+             kKp5] `zip` repeat True) ++ [(96, False), (126, False)]))
 
     undefined -- TODO
 
