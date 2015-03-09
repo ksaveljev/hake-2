b8a2b1d2ffe079ae616cb8e1cfd009cd99fee0a3
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-09 17:37:56 +0200

    Start some work on FS.initFileSystem

--- src/QCommon/FS.hs
+++ src/QCommon/FS.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module QCommon.FS where
 
 import Quake
+import Internal
+import qualified Game.Cmd as Cmd
 
 initFileSystem :: Quake ()
-initFileSystem = undefined -- TODO
+initFileSystem = do
+    Cmd.addCommand "path" pathF
+    Cmd.addCommand "link" linkF
+    Cmd.addCommand "dir" dirF
+
+    undefined -- TODO
+
+pathF :: XCommandT
+pathF = undefined -- TODO
+
+linkF :: XCommandT
+linkF = undefined -- TODO
+
+dirF :: XCommandT
+dirF = undefined -- TODO
