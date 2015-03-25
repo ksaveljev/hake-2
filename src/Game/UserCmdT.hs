fe1d1af38dc116ee7191c7a685de1126ff98ca61
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-25 17:02:19 +0200

    UserCmdT.newUserCmdT

--- src/Game/UserCmdT.hs
+++ src/Game/UserCmdT.hs
@@ -2,7 +2,7 @@
 module Game.UserCmdT where
 
 import Data.Int (Int8, Int16)
-import Linear.V3 (V3)
+import Linear.V3 (V3(..))
 import Control.Lens (makeLenses)
 
 data UserCmdT =
@@ -19,4 +19,13 @@ data UserCmdT =
 makeLenses ''UserCmdT
 
 newUserCmdT :: UserCmdT
-newUserCmdT = undefined -- TODO
+newUserCmdT =
+  UserCmdT { _ucMsec        = 0
+           , _ucButtons     = 0
+           , _ucAngles      = V3 0 0 0
+           , _ucForwardMove = 0
+           , _ucSideMove    = 0
+           , _ucUpMove      = 0
+           , _ucImpulse     = 0
+           , _ucLightLevel  = 0
+           }
