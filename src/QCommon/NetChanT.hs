daa6014f4261748a76d6bfa17e1e748213cf3456
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-03-25 17:09:09 +0200

    NetChanT.newNetChanT

--- src/QCommon/NetChanT.hs
+++ src/QCommon/NetChanT.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module QCommon.NetChanT where
 
-import Data.Word (Word8)
 import Control.Lens (makeLenses)
-import qualified Data.Vector.Unboxed as UV
+import qualified Data.ByteString as B
 
 import QCommon.NetAdrT
 import QCommon.SizeBufT
 
 data NetChanT =
   NetChanT { _ncFatalError                   :: Bool
-           , _ncSock                         :: Int
+           , _ncSock                         :: Int -- TODO: is it Int or Socket??
            , _ncDropped                      :: Int
            , _ncLastReceived                 :: Int
            , _ncLastSent                     :: Int
@@ -23,12 +23,30 @@ data NetChanT =
            , _ncOutgoingSequence             :: Int
            , _ncLastReliableSequence         :: Int
            , _ncMessage                      :: SizeBufT
-           , _ncMessageBuf                   :: UV.Vector Word8
+           , _ncMessageBuf                   :: B.ByteString
            , _ncReliableLength               :: Int
-           , _ncReliableBuf                  :: UV.Vector Word8
+           , _ncReliableBuf                  :: B.ByteString
            }
 
 makeLenses ''NetChanT
 
 newNetChanT :: NetChanT
-newNetChanT = undefined -- TODO
+newNetChanT =
+  NetChanT { _ncFatalError                   = False
+           , _ncSock                         = 0
+           , _ncDropped                      = 0
+           , _ncLastReceived                 = 0
+           , _ncLastSent                     = 0
+           , _ncRemoteAddress                = newNetAdrT
+           , _ncRemoteQPort                  = 0
+           , _ncIncomingSequence             = 0
+           , _ncIncomingAcknowledged         = 0
+           , _ncIncomingReliableAcknowledged = 0
+           , _ncIncomingReliableSequence     = 0
+           , _ncOutgoingSequence             = 0
+           , _ncLastReliableSequence         = 0
+           , _ncMessage                      = newSizeBufT
+           , _ncMessageBuf                   = "" -- size [Constants.maxMsgLen - 16]
+           , _ncReliableLength               = 0
+           , _ncReliableBuf                  = "" -- size [Constants.maxMsgLen - 16]
+           }
