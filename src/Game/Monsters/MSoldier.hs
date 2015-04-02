6620a1f1b2a0ce4ddb28649edaa66aa7242a52e6
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-02 11:38:34 +0300

    MSoldier.spMonsterSoldier

--- src/Game/Monsters/MSoldier.hs
+++ src/Game/Monsters/MSoldier.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Game.Monsters.MSoldier where
 
-import Control.Lens ((^.), (.=), use, ix, zoom)
+import Control.Lens ((^.), (.=), use, ix, zoom, preuse)
 import Control.Monad (liftM, void)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
 
 import Quake
 import QuakeState
 import CVarVariables
 import Game.EntThink
 import qualified Game.GameUtil as GameUtil
+import qualified QCommon.Com as Com
 
 spMonsterSoldierX :: EntThink
 spMonsterSoldierX =
@@ -17,8 +20,30 @@ spMonsterSoldierX =
 
 spMonsterSoldier :: EntThink
 spMonsterSoldier =
-  GenericEntThink "SP_monster_soldier" $ \_ -> do
-    io (putStrLn "MSoldier.spMonsterSoldier") >> undefined -- TODO
+  GenericEntThink "SP_monster_soldier" $ \er@(EdictReference edictIdx) -> do
+    Just edict <- preuse $ gameBaseGlobals.gbGEdicts.ix edictIdx
+
+    Com.dprintf $ "Spawning a soldier at " `B.append` BC.pack (show (edict^.eEntityState.esOrigin)) -- IMPROVE
+
+    deathmatchValue <- liftM (^.cvValue) deathmatchCVar
+
+    if deathmatchValue /= 0
+      then GameUtil.freeEdict er
+      else do
+        void $ think spMonsterSoldierX er
+
+        soundIndex <- use $ gameBaseGlobals.gbGameImport.giSoundIndex
+
+        soundIndex "soldier/solpain1.wav" >>= (mSoldierGlobals.msSoundPain .=)
+        soundIndex "soldier/soldeth1.wav" >>= (mSoldierGlobals.msSoundDeath .=)
+        void $ soundIndex "soldier/solatck1.wav"
+
+        zoom (gameBaseGlobals.gbGEdicts.ix edictIdx) $ do
+          eEntityState.esSkinNum .= 2
+          eEdictStatus.eHealth .= 30
+          eEdictStatus.eGibHealth .= (-30)
+
+    return True
 
 spMonsterSoldierSS :: EntThink
 spMonsterSoldierSS =
