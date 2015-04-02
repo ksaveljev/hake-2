4d58a58dc28cf0b2b9777f5e6acc27c784ff671c
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-02 10:45:41 +0300

    MSoldier.spMonsterSoldierLight

--- src/Game/Monsters/MSoldier.hs
+++ src/Game/Monsters/MSoldier.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Game.Monsters.MSoldier where
 
-import Control.Lens ((^.), (.=), use, zoom, ix)
+import Control.Lens ((^.), (.=), use, ix)
 import Control.Monad (liftM, void)
 
 import Quake
@@ -45,12 +45,16 @@ spMonsterSoldierLight =
         void $ soundIndex "misc/lasfly.wav"
         void $ soundIndex "soldier/solatck2.wav"
 
-        io (putStrLn "spMonsterSoldierLight") >> undefined -- TODO
-        {- ZOOM DOESN'T WORK FOR ME RIGHT NOW
+        {- IMPROVE: ZOOM
         zoom (gameBaseGlobals.gbGEdicts.ix edictIdx) $ do
           eEntityState.esSkinNum .= 0
           eEdictStatus.eHealth .= 20
           eEdictStatus.eGibHealth .= (-30)
           -}
+
+        let edictLens = gameBaseGlobals.gbGEdicts.ix edictIdx
+        edictLens.eEntityState.esSkinNum .= 0
+        edictLens.eEdictStatus.eHealth .= 20
+        edictLens.eEdictStatus.eGibHealth .= (-30)
 
     return True
