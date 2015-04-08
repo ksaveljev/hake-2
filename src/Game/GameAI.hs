7e8fbcda5b0478a4276b2d9b455ed75cd37c6375
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-08 14:38:35 +0300

    GameAI.aiSetSightClient

--- src/Game/GameAI.hs
+++ src/Game/GameAI.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf #-}
 module Game.GameAI where
 
+import Control.Lens (use, (^.), ix, preuse, (.=))
+import Data.Bits ((.&.))
+import Data.Maybe (isNothing)
+
 import Quake
+import QuakeState
 import Game.Adapters
+import qualified Constants
 
 aiStand :: AI
 aiStand =
@@ -34,5 +41,38 @@ walkMonsterStart =
   GenericEntThink "walkmonster_start" $ \_ -> do
     io (putStrLn "GameAI.walkMonsterStart") >> undefined -- TODO
 
+{-
+- Called once each frame to set level.sight_client to the player to be
+- checked for in findtarget.
+- 
+- If all clients are either dead or in notarget, sight_client will be null.
+- 
+- In coop games, sight_client will cycle between the clients.
+-}
 aiSetSightClient :: Quake ()
-aiSetSightClient = io (putStrLn "GameAI.aiSetSightClient") >> undefined -- TODO
+aiSetSightClient = do
+    sightClient <- use $ gameBaseGlobals.gbLevel.llSightClient
+
+    let start = if isNothing sightClient
+                  then 1
+                  else let Just (EdictReference idx) = sightClient
+                       in idx
+        check = start
+
+    maxClientsValue <- use $ gameBaseGlobals.gbGame.glMaxClients
+
+    lookThroughClients maxClientsValue start check
+
+  where lookThroughClients :: Int -> Int -> Int -> Quake ()
+        lookThroughClients maxClients start check = do
+          let check' = if check + 1 > maxClients
+                         then 1
+                         else check + 1
+
+          Just edict <- preuse $ gameBaseGlobals.gbGEdicts.ix check'
+
+          if | (edict^.eInUse) && (edict^.eEdictStatus.eHealth) > 0 && (edict^.eFlags) .&. Constants.flNoTarget == 0 ->
+                 gameBaseGlobals.gbLevel.llSightClient .= Just (EdictReference check') -- got one
+             | check' == start ->
+                 gameBaseGlobals.gbLevel.llSightClient .= Nothing
+             | otherwise -> lookThroughClients maxClients start check'
