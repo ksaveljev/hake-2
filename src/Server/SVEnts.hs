26a332693279cde78b02d1527141872536b89510
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-13 20:46:45 +0300

    Skip SVEnts.recordDemoMessage

--- src/Server/SVEnts.hs
+++ src/Server/SVEnts.hs
@@ -1,10 +1,19 @@
 module Server.SVEnts where
 
+import Control.Lens (use)
+import Control.Monad (when)
+import Data.Maybe (isJust)
+
 import Quake
+import QuakeState
 
 {-
 - Save everything in the world out without deltas. Used for recording
 - footage for merged or assembled demos.
 -}
 recordDemoMessage :: Quake ()
-recordDemoMessage = io (putStrLn "SVEnts.recordDemoMessage") >> undefined -- TODO
+recordDemoMessage = do
+    demoFile <- use $ svGlobals.svServerStatic.ssDemoFile
+
+    when (isJust demoFile) $ do
+      io (putStrLn "SVEnts.recordDemoMessage") >> undefined -- TODO
