0e471ada8586c94bc2a128f5a11f0274ec26cc2c
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-20 15:16:02 +0300

    Menu.center

--- src/Client/Menu.hs
+++ src/Client/Menu.hs
@@ -1,6 +1,9 @@
 module Client.Menu where
 
+import Control.Lens (use, preuse, ix, (.=))
+
 import Quake
+import QuakeState
 
 init :: Quake ()
 init = io (putStrLn "Menu.init") >> undefined -- TODO
@@ -11,4 +14,8 @@ addItem _ _ = io (putStrLn "Menu.addItem") >> undefined -- TODO
 
 -- TODO: IMPROVE: instead of Int use "Reference" newtype ?
 center :: Int -> Quake ()
-center _ = io (putStrLn "Menu.center") >> undefined -- TODO
+center menuIdx = do
+    Just height <- preuse $ menuGlobals.mgMenuFrameworks.ix menuIdx.mfY
+    h <- use $ globals.vidDef.vdHeight
+
+    menuGlobals.mgMenuFrameworks.ix menuIdx.mfY .= (h - (height + 10)) `div` 2
