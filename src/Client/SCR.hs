bbf9ddc4f8d31020158b4f72df12af9d44d3ea24
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-24 12:28:54 +0300

    SCR.init

--- src/Client/SCR.hs
+++ src/Client/SCR.hs
@@ -3,19 +3,44 @@
 module Client.SCR where
 
 import Control.Lens ((.=), use, (^.))
-import Control.Monad (liftM, when)
+import Control.Monad (liftM, when, void)
 
 import Quake
 import QuakeState
 import CVarVariables
 import QCommon.XCommandT
 import qualified Constants
+import {-# SOURCE #-} qualified Game.Cmd as Cmd
 import qualified QCommon.Com as Com
+import qualified QCommon.CVar as CVar
 import qualified Sound.S as S
 import qualified Sys.Timer as Timer
 
 init :: Quake ()
-init = io (putStrLn "SCR.init") >> undefined -- TODO
+init = do
+    void $ CVar.get "viewsize" "100" Constants.cvarArchive
+    void $ CVar.get "scr_conspeed" "3" 0
+    void $ CVar.get "scr_showturtle" "0" 0
+    void $ CVar.get "scr_showpause" "1" 0
+    void $ CVar.get "scr_centertime" "2.5" 0
+    void $ CVar.get "scr_printspeed" "8" 0
+    void $ CVar.get "netgraph" "1" 0
+    void $ CVar.get "timegraph" "1" 0
+    void $ CVar.get "debuggraph" "1" 0
+    void $ CVar.get "graphheight" "32" 0
+    void $ CVar.get "graphscale" "1" 0
+    void $ CVar.get "graphshift" "0" 0
+    void $ CVar.get "scr_drawall" "1" 0
+    void $ CVar.get "fps" "0" 0
+
+    -- register our commands
+    Cmd.addCommand "timerefresh" (Just timeRefreshF)
+    Cmd.addCommand "loading" (Just loadingF)
+    Cmd.addCommand "sizeup" (Just sizeUpF)
+    Cmd.addCommand "sizedown" (Just sizeDownF)
+    Cmd.addCommand "sky" (Just skyF)
+
+    scrGlobals.scrInitialized .= True
 
 beginLoadingPlaque :: Quake ()
 beginLoadingPlaque = do
@@ -84,3 +109,18 @@ updateScreen2 = do
                -- not initialized yet
              | (not initialized) || (not conInitialized) -> return False
              | otherwise -> return True
+
+timeRefreshF :: XCommandT
+timeRefreshF = io (putStrLn "SCR.timeRefreshF") >> undefined -- TODO
+
+loadingF :: XCommandT
+loadingF = io (putStrLn "SCR.loadingF") >> undefined -- TODO
+
+sizeUpF :: XCommandT
+sizeUpF = io (putStrLn "SCR.sizeUpF") >> undefined -- TODO
+
+sizeDownF :: XCommandT
+sizeDownF = io (putStrLn "SCR.sizeDownF") >> undefined -- TODO
+
+skyF :: XCommandT
+skyF = io (putStrLn "SCR.skyF") >> undefined -- TODO
