874434d584c199659ad10d3402df2df9d333cb30
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-04-29 17:40:28 +0300

    Draw.findPic

--- src/Render/Fast/Draw.hs
+++ src/Render/Fast/Draw.hs
@@ -2,10 +2,13 @@
 module Render.Fast.Draw where
 
 import Control.Lens (preuse, (^.), ix)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
 import qualified Graphics.Rendering.OpenGL.Raw as GL
 
 import Quake
 import QuakeState
+import Render.OpenGL.GLDriver
 import qualified Render.Fast.Image as Image
 import qualified Render.RenderAPIConstants as RenderAPIConstants
 
@@ -17,3 +20,13 @@ initLocal = do
     Image.glBind (image^.iTexNum)
     GL.glTexParameterf GL.gl_TEXTURE_2D GL.gl_TEXTURE_MIN_FILTER (fromIntegral GL.gl_NEAREST)
     GL.glTexParameterf GL.gl_TEXTURE_2D GL.gl_TEXTURE_MAG_FILTER (fromIntegral GL.gl_NEAREST)
+
+stretchPic :: GLDriver -> Int -> Int -> Int -> Int -> B.ByteString -> Quake ()
+stretchPic glDriver x y w h pic = do
+    io (putStrLn "FastRenderAPI.fastDrawStretchPic") >> undefined -- TODO
+
+findPic :: B.ByteString -> Quake (Maybe ImageReference)
+findPic name =
+    if BC.take 1 name == "/" || BC.take 1 name == "\\"
+      then Image.glFindImage (B.drop 1 name) RenderAPIConstants.itPic
+      else Image.glFindImage name RenderAPIConstants.itPic
