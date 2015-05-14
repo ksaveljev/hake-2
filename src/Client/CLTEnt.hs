bb198ae4aa7ce1ba3e4c2afad8564672a7210dc4
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-05-15 00:27:59 +0300

    CLTEnt.clearTEnts

--- src/Client/CLTEnt.hs
+++ src/Client/CLTEnt.hs
@@ -1,11 +1,20 @@
 module Client.CLTEnt where
 
+import Control.Lens (zoom, (.=))
+import qualified Data.Vector as V
+
 import Quake
+import QuakeState
+import qualified Constants
 
 clearTEnts :: Quake ()
 clearTEnts = do
-    io (putStrLn "IMPLEMENT ME!! CLTEnt.clearTEnts") >> return ()
-    -- io (putStrLn "CLTEnt.clearTEnts") >> undefined -- TODO
+    zoom clTEntGlobals $ do
+      clteExplosions  .= V.replicate Constants.maxExplosions newExplosionT
+      clteBeams       .= V.replicate Constants.maxBeams newBeamT
+      cltePlayerBeams .= V.replicate Constants.maxBeams newBeamT
+      clteLasers      .= V.replicate Constants.maxLasers newLaserT
+      clteSustains    .= V.replicate Constants.maxSustains newCLSustainT
 
 registerTEntModels :: Quake ()
 registerTEntModels = do
