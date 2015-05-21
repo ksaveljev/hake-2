b048c44be247ec4b320defeccc2ce7277b0f81b2
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-05-21 08:36:37 +0300

    M.checkGround

--- src/Client/M.hs
+++ src/Client/M.hs
@@ -2,8 +2,9 @@
 module Client.M where
 
 import Control.Lens (zoom, (.=), preuse, ix, (^.), use, (%=))
-import Control.Monad (unless)
+import Control.Monad (unless, when)
 import Data.Bits ((.|.), (.&.), complement)
+import Linear (V3(..), _z)
 
 import Quake
 import QuakeState
@@ -12,7 +13,40 @@ import qualified Constants
 import qualified Util.Lib as Lib
 
 checkGround :: EdictReference -> Quake ()
-checkGround _ = io (putStrLn "M.checkGround") >> undefined -- TODO
+checkGround edictRef@(EdictReference edictIdx) = do
+    Just edict <- preuse $ gameBaseGlobals.gbGEdicts.ix edictIdx
+
+    unless ((edict^.eFlags) .&. (Constants.flSwim .|. Constants.flFly) /= 0) $ do
+      if (edict^.eEdictPhysics.eVelocity._z) > 100
+        then
+          gameBaseGlobals.gbGEdicts.ix edictIdx.eEdictOther.eoGroundEntity .= Nothing
+        else do
+          -- if the hull point one-quarter unit down is solid the entity is
+          -- on ground
+          let point = let V3 a b c = edict^.eEntityState.esOrigin in V3 a b (c - 0.25)
+
+          trace <- use $ gameBaseGlobals.gbGameImport.giTrace
+          traceT <- trace (edict^.eEntityState.esOrigin) 
+                         (Just $ edict^.eEdictMinMax.eMins)
+                         (Just $ edict^.eEdictMinMax.eMaxs)
+                         point
+                         edictRef
+                         Constants.maskMonsterSolid
+
+          -- check steepness
+          if (traceT^.tPlane.cpNormal._z) < 0.7 && not (traceT^.tStartSolid)
+            then
+              gameBaseGlobals.gbGEdicts.ix edictIdx.eEdictOther.eoGroundEntity .= Nothing
+            else do
+              when (not (traceT^.tStartSolid) && not (traceT^.tAllSolid)) $ do
+                let Just (EdictReference traceIdx) = traceT^.tEnt
+                Just linkCount <- preuse $ gameBaseGlobals.gbGEdicts.ix traceIdx.eLinkCount
+
+                zoom (gameBaseGlobals.gbGEdicts.ix edictIdx) $ do
+                  eEntityState.esOrigin .= traceT^.tEndPos
+                  eEdictOther.eoGroundEntity .= traceT^.tEnt
+                  eGroundEntityLinkCount .= linkCount
+                  eEdictPhysics.eVelocity._z .= 0
 
 -- Stops the Flies.
 fliesOff :: EntThink
