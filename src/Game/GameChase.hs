7c059acaf7b7b0ed871969be558893d8a569412a
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-11-11 14:07:30 +0200

    GameChase.getChaseTarget

--- src/Game/GameChase.hs
+++ src/Game/GameChase.hs
@@ -1,11 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Game.GameChase where
+
+import Control.Lens (use, (^.), (.=), zoom, ix, preuse)
+import Control.Monad (unless, liftM)
 
 import Quake
 import QuakeState
+import CVarVariables
 
 getChaseTarget :: EdictReference -> Quake ()
-getChaseTarget _ = do
-    io (putStrLn "GameChase.getChaseTarget") >> undefined -- TODO
+getChaseTarget edictRef = do
+    maxClientsValue <- liftM (^.cvValue) maxClientsCVar
+    done <- findChaseTarget 1 (truncate maxClientsValue)
+
+    unless done $ do
+      centerPrintf <- use $ gameBaseGlobals.gbGameImport.giCenterPrintf
+      centerPrintf edictRef "No other players to chase."
+
+  where findChaseTarget :: Int -> Int -> Quake Bool
+        findChaseTarget idx maxIdx
+          | idx > maxIdx = return False
+          | otherwise = do
+              let otherRef = newEdictReference idx
+              other <- readEdictT otherRef
+              let Just (GClientReference gClientIdx) = other^.eClient
+              Just gClient <- preuse $ gameBaseGlobals.gbGame.glClients.ix gClientIdx
+
+              if (other^.eInUse) && not (gClient^.gcResp.crSpectator)
+                then do
+                  zoom (gameBaseGlobals.gbGame.glClients.ix gClientIdx) $ do
+                    gcChaseTarget .= Just otherRef
+                    gcUpdateChase .= True
+
+                  updateChaseCam edictRef
+                  return True
+
+                else
+                  findChaseTarget (idx + 1) maxIdx
 
 updateChaseCam :: EdictReference -> Quake ()
 updateChaseCam _ = do
