bd5582d0fc03e4bef3124678ab91e75f20ab98f1
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2016-02-21 00:53:27 +0200

    Reduce duplication for FS module (fOpenFile and fileLength)

--- src/QCommon/FSShared.hs
+++ src/QCommon/FSShared.hs
@@ -8,13 +8,14 @@ import qualified QCommon.Com as Com
 import           QuakeState
 import           Types
 
-import           Control.Lens (use, (.=), (^.))
+import           Control.Lens (use, (.=), (^.), (%=), (&), (.~))
+import           Control.Monad (when)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import           Data.Char (toLower)
 import           Data.Foldable (find)
 import qualified Data.HashMap.Lazy as HM
-import           System.IO (Handle, hClose)
+import           System.IO (Handle, hClose, hIsOpen, openBinaryFile, hSeek, IOMode(ReadMode), SeekMode(AbsoluteSeek))
 import           System.Directory
 import           System.PosixCompat.Files (getFileStatus, fileSize)
 
@@ -26,14 +27,12 @@ loadFile path =
     if (index != -1)
         path = path.substring(0, index); -}
   do len <- fileLength path
-     openAndRead path len
+     maybe (return Nothing) (openAndRead path) len
 
 openAndRead :: B.ByteString -> Int -> Quake (Maybe B.ByteString)
-openAndRead path len
-  | len < 1 = return Nothing
-  | otherwise =
-      do fileHandle <- fOpenFile path
-         maybe openFailed readContents fileHandle
+openAndRead path len =
+  do fileHandle <- fOpenFile path
+     maybe openFailed readContents fileHandle
   where openFailed =
           do Com.comError Constants.errFatal "LoadFile failed"
              return Nothing
@@ -43,36 +42,74 @@ openAndRead path len
                       io (hClose fileHandle)
                       return (Just contents))
 
-fileLength :: B.ByteString -> Quake Int
+fOpenFile :: B.ByteString -> Quake (Maybe Handle)
+fOpenFile fileName =
+  searchForFile fileName fromFileLink fromPackFile fromDirectoryTree fileNotFound
+  where fromFileLink netPath True = fromDirectoryTree netPath
+        fromFileLink _ False = return Nothing
+        fromPackFile pack entry True =
+          request (io (do h <- openBinaryFile (BC.unpack (pack^.pFilename)) ReadMode
+                          hSeek h AbsoluteSeek (fromIntegral (entry^.pfFilePos))
+                          return (Just h)))
+        fromPackFile pack _ False =
+          do Com.fatalError ("Couldn't reopen " `B.append` (pack^.pFilename))
+             return Nothing
+        fromDirectoryTree netPath =
+          do h <- request (io (openBinaryFile (BC.unpack netPath) ReadMode))
+             return (Just h)
+        fileNotFound = return Nothing
+
+fileLength :: B.ByteString -> Quake (Maybe Int)
 fileLength fileName =
+  searchForFile fileName fromFileLink fromPackFile fromDirectoryTree fileNotFound
+  where fromFileLink netPath True =
+          do Com.dprintf (B.concat ["link file: ", netPath, "\n"])
+             fmap (Just . fromIntegral) (getFileSize netPath)
+        fromFileLink _ False = return Nothing
+        fromPackFile _ entry True = return (Just (entry^.pfFileLen))
+        fromPackFile pack _ False =
+          do Com.fatalError ("Couldn't reopen " `B.append` (pack^.pFilename))
+             return Nothing
+        fromDirectoryTree netPath =
+          do Com.dprintf (B.concat ["FindFile: ", netPath, "\n"])
+             fmap (Just . fromIntegral) (getFileSize netPath)
+        fileNotFound =
+          do Com.dprintf (B.concat ["FindFile: can't find ", fileName, "\n"])
+             return Nothing
+        getFileSize netPath = request (io (fmap fileSize (getFileStatus (BC.unpack netPath))))
+
+searchForFile :: B.ByteString
+              -> (B.ByteString -> Bool -> Quake (Maybe a))
+              -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+              -> (B.ByteString -> Quake (Maybe a))
+              -> Quake (Maybe a)
+              -> Quake (Maybe a)
+searchForFile fileName fromFileLink fromPackFile fromDirectoryTree fileNotFound =
   do fsGlobals.fsFileFromPak .= 0
      links <- use (fsGlobals.fsLinks)
-     maybe (fileLinkNotFound fileName) (fileLinkFound fileName) (findLink links)
+     maybe (fileLinkNotFound fileName fromPackFile fromDirectoryTree fileNotFound)
+           (fileLinkFound fileName fromFileLink)
+           (findLink links)
   where findLink = find (fileLinkMatch fileName)
 
-fileLinkNotFound :: B.ByteString -> Quake Int
-fileLinkNotFound fileName =
+fileLinkNotFound :: B.ByteString
+                 -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+                 -> (B.ByteString -> Quake (Maybe a))
+                 -> Quake (Maybe a)
+                 -> Quake (Maybe a)
+fileLinkNotFound fileName fromPackFile fromDirectoryTree fileNotFound =
   do searchPaths <- use (fsGlobals.fsSearchPaths)
-     foundFileLen <- searchPathMatch fileName searchPaths
-     maybe fileNotFound return foundFileLen
-  where fileNotFound =
-          do Com.dprintf (B.concat ["FindFile: can't find ", fileName, "\n"])
-             return (-1)
+     searchResult <- searchPathMatch fileName searchPaths fromPackFile fromDirectoryTree
+     maybe fileNotFound (return . Just) searchResult
 
-fileLinkFound :: B.ByteString -> FileLinkT -> Quake Int
-fileLinkFound fileName fileLink =
-  do havePermissions <- request (io (canRead netPathStr))
-     getFileSize havePermissions
+fileLinkFound :: B.ByteString
+              -> (B.ByteString -> Bool -> Quake (Maybe a))
+              -> FileLinkT
+              -> Quake (Maybe a)
+fileLinkFound fileName fromFileLink fileLink =
+  do havePermissions <- request (io (canRead (BC.unpack netPath)))
+     fromFileLink netPath havePermissions
   where netPath = (fileLink^.flTo) `B.append` B.take (fileLink^.flFromLength) fileName
-        netPathStr = BC.unpack netPath
-        getFileSize True =
-          do Com.dprintf (B.concat ["link file: ", netPath, "\n"])
-             size <- request (io (fmap fileSize (getFileStatus netPathStr)))
-             return (fromIntegral size)
-        getFileSize False = return (-1)
-
-fOpenFile :: B.ByteString -> Quake (Maybe Handle)
-fOpenFile = error "FSShared.fOpenFile"
 
 canRead :: String -> IO Bool
 canRead file =
@@ -86,42 +123,61 @@ fileLinkMatch name fileLink =
       len = fileLink^.flFromLength
   in B.take len name == B.take len from
 
-searchPathMatch :: B.ByteString -> [SearchPathT] -> Quake (Maybe Int)
-searchPathMatch _ [] = return Nothing
-searchPathMatch name searchPaths@(searchPath:xs) =
-  maybe (checkDirectoryTree name searchPaths)
-        (processPackFile name xs)
+searchPathMatch :: B.ByteString
+                -> [SearchPathT]
+                 -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+                 -> (B.ByteString -> Quake (Maybe a))
+                -> Quake (Maybe a)
+searchPathMatch _ [] _ _ = return Nothing
+searchPathMatch name searchPaths@(searchPath:xs) fromPackFile fromDirectoryTree =
+  maybe (checkDirectoryTree name searchPaths fromPackFile fromDirectoryTree)
+        (processPackFile name xs fromPackFile fromDirectoryTree)
         (searchPath^.spPack)
 
-checkDirectoryTree :: B.ByteString -> [SearchPathT] -> Quake (Maybe Int)
-checkDirectoryTree _ [] =
+checkDirectoryTree :: B.ByteString
+                   -> [SearchPathT]
+                   -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+                   -> (B.ByteString -> Quake (Maybe a))
+                   -> Quake (Maybe a)
+checkDirectoryTree _ [] _ _ =
   do Com.fatalError "FS.checkDirectoryTree illegal state"
      return Nothing
-checkDirectoryTree name (searchPath:xs) =
-  do havePermissions <- request (io (canRead netPathStr))
-     getFileSize havePermissions
+checkDirectoryTree name (searchPath:xs) fromPackFile fromDirectoryTree =
+  do havePermissions <- request (io (canRead (BC.unpack netPath)))
+     getResult havePermissions
   where netPath = B.concat [searchPath^.spFilename, "/", name]
-        netPathStr = BC.unpack netPath
-        getFileSize True =
-          do Com.dprintf (B.concat ["FindFile: ", netPath, "\n"])
-             size <- request (io (fmap fileSize (getFileStatus netPathStr)))
-             return (Just (fromIntegral size))
-        getFileSize False = searchPathMatch name xs
+        getResult True = fromDirectoryTree netPath
+        getResult False = searchPathMatch name xs fromPackFile fromDirectoryTree
 
-processPackFile :: B.ByteString -> [SearchPathT] -> PackT -> Quake (Maybe Int)
-processPackFile name searchPaths pack =
-  maybe proceedSearching (packFileFound fileName pack) (HM.lookup fileName files)
+processPackFile :: B.ByteString
+                -> [SearchPathT]
+                 -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+                 -> (B.ByteString -> Quake (Maybe a))
+                -> PackT
+                -> Quake (Maybe a)
+processPackFile _ [] _ _ _ =
+  do Com.fatalError "FS.processPackFile illegal state"
+     return Nothing
+processPackFile name searchPaths@(searchPath:_) fromPackFile fromDirectoryTree pack =
+  do needsUpdate <- maybe (return True) needToOpen (pack^.pHandle)
+     when needsUpdate openPackHandle
+     maybe proceedSearching (packFileFound fileName pack fromPackFile) (HM.lookup fileName files)
   where fileName = BC.map toLower name
         files = pack^.pFiles
-        proceedSearching = searchPathMatch name searchPaths
+        proceedSearching = searchPathMatch name searchPaths fromPackFile fromDirectoryTree
+        needToOpen h = fmap not (request (io (hIsOpen h)))
+        openPackHandle =
+          do h <- request (io (openBinaryFile (BC.unpack (pack^.pFilename)) ReadMode))
+             fsGlobals.fsSearchPaths %= map (\sp -> if sp == searchPath then updatedSp h else sp) -- IMPROVE? any way to do it better?
+        updatedSp h = searchPath & spPack .~ Just (pack & pHandle .~ Just h)
 
-packFileFound :: B.ByteString -> PackT -> PackFileT -> Quake (Maybe Int)
-packFileFound fileName pack entry =
+packFileFound :: B.ByteString
+              -> PackT
+              -> (PackT -> PackFileT -> Bool -> Quake (Maybe a))
+              -> PackFileT
+              -> Quake (Maybe a)
+packFileFound fileName pack fromPackFile entry =
   do fsGlobals.fsFileFromPak .= 1
      Com.dprintf (B.concat ["PackFile: ", pack^.pFilename, " : ", fileName, "\n"])
      havePermissions <- request (io (canRead (BC.unpack (pack^.pFilename))))
-     getFileSize havePermissions
-  where getFileSize True = return (Just (entry^.pfFileLen))
-        getFileSize False =
-          do Com.fatalError ("Couldn't reopen " `B.append` (pack^.pFilename))
-             return Nothing
+     fromPackFile pack entry havePermissions
